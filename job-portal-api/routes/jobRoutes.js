2c23f808a607ab5eb9ca3948691042d514f2eac1
Y4NN777 <axel.studiesmail@gmail.com>
2025-06-02 19:48:22 +0000

    Adding the jobs specific endpoints

---
+++ job-portal-api/routes/jobRoutes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const {
+  createJob,
+  getAllJobs,
+  getJobById,
+  updateJob,
+  deleteJob
+} = require('../controllers/jobController');
+const { protect, authorize } = require('../middlewares/auth');
+
+const router = express.Router();
+
+router.route('/')
+  .get(getAllJobs)
+  .post(protect, authorize('recruiter', 'admin'), createJob);
+
+router.route('/:id')
+  .get(getJobById)
+  .put(protect, authorize('recruiter', 'admin'), updateJob)
+  .delete(protect, authorize('recruiter', 'admin'), deleteJob);
+
+module.exports = router;
